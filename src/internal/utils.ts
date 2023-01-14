09404e9ac79823b152e9ad74794492b2d1b15bda
Jianwei Jiang <a370109945@gmail.com>
2023-01-14 22:55:41 +0800

    Add `delay` function to util.ts

--- src/internal/utils.ts
+++ src/internal/utils.ts
@@ -1,5 +1,7 @@
 import { BigIntConstants } from "./constants";
 
+export const delay = (ms: number) => { return new Promise(resolve => setTimeout(resolve, ms)) };
+
 export const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
     arr.reduce((groups, item) => {
         (groups[key(item)] ||= []).push(item);
